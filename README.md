710bed58004f73d9543f1ca65a171ee5e2680edb
Cadu <cadu.coelho@gmail.com>
2024-11-14 23:31:55 -0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,21 @@
+# @main
+
+`@main` decorator which runs the tagged function if the current module is being run.
+
+No more `if __name__ == \"__main__\":` all over the place.
+
+That's it!
+
+### Usage
+```python
+from main import main
+
+A = 10
+B = 20
+
+
+@main
+def do_print():
+    """This will run if this module is called directly."""
+    print(A + B)
+```
---
+++ tests/test_basic.py
@@ -0,0 +1,49 @@
+import pytest
+
+from main import main
+
+
+class NotSet:
+    pass
+
+
+EXIT_CODE_RECEIVED = NotSet
+
+
+@pytest.fixture
+def mock_exit():
+    import builtins
+
+    original_exit = builtins.exit
+
+    def mock_exit(code):
+        global EXIT_CODE_RECEIVED
+        EXIT_CODE_RECEIVED = code
+
+    # Apply Mock
+    builtins.exit = mock_exit
+
+    yield
+
+    # Clean up
+    builtins.exit = original_exit
+    EXIT_CODE_RECEIVED = NotSet
+
+
+def test_assert_function_actually_gets_called(mock_exit):
+    """
+    Assert that the @main decorator actually calls the function if the module is being run as a script.
+    """
+
+    # We patch __name__ here because doing so via a proper pytest fixture would be _A Lot Of Work (TM)_, because
+    # of the insane amount of stack manipulation that would be required to get the desired effect.
+
+    @main
+    def my_main_func():
+        """
+        The answer to life, the universe, and everything.
+        """
+        return 42
+
+    # Ensure that our main function was able to call mock_exit with the expected value.
+    assert EXIT_CODE_RECEIVED == 42
