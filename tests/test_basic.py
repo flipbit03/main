2a8a86bdb48a0fac92b8a0327b90ead16d36a3dd
Cadu <cadu.coelho@gmail.com>
2024-11-16 13:57:55 -0300

    Fix tests (again)

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -3,15 +3,11 @@ import pytest
 from python_main import main
 import builtins
 
-class NotSet:
-    pass
-
-
-EXIT_CODE_RECEIVED = NotSet
-
+EXIT_CODE_RECEIVED = -1
 
 @pytest.fixture
 def mock_exit():
+    global EXIT_CODE_RECEIVED
     original_exit = builtins.exit
 
     def mock_exit(code):
@@ -25,7 +21,7 @@ def mock_exit():
 
     # Clean up
     builtins.exit = original_exit
-    EXIT_CODE_RECEIVED = NotSet
+    EXIT_CODE_RECEIVED = -1
 
 
 def __my_main_func():
@@ -48,10 +44,12 @@ def test_assert_function_actually_gets_called(mock_exit):
     main(__my_main_func)
 
     # Ensure that our main function was able to call mock_exit with the expected value.
+    global EXIT_CODE_RECEIVED
     assert EXIT_CODE_RECEIVED == 42
 
     # Restore
     __my_main_func.__module__ = __my_main_func_original_module
+
 
 
 def test_assert_function_does_not_get_called(mock_exit):
@@ -63,4 +61,5 @@ def test_assert_function_does_not_get_called(mock_exit):
     main(__my_main_func)
 
     # Exit code will not have been set.
-    assert EXIT_CODE_RECEIVED == NotSet
+    global EXIT_CODE_RECEIVED
+    assert EXIT_CODE_RECEIVED == -1
