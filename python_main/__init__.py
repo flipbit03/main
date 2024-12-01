ba1808625aee04e4d280c6cf29ef65cc2338eded
cadu <cadu.coelho@gmail.com>
2024-12-01 19:30:46 -0300

    Add py.typed marker, improve typing, make release process automatically set package version

--- python_main/__init__.py
+++ python_main/__init__.py
@@ -1,10 +1,16 @@
-from typing import Callable, Optional
+from typing import Any, Callable, TypeVar
 
 __RAN_AS_SCRIPT_MODULE = "__main__"
 __CALLABLE_MODULE_PROP = "__module__"
 
+__MAIN_RETURN_TYPE = TypeVar("__MAIN_RETURN_TYPE", bound=Any)
 
-def main(f: Callable[[], Optional[int]]) -> Callable:
+
+def main(f: Callable[[], __MAIN_RETURN_TYPE]) -> Callable[[], __MAIN_RETURN_TYPE]:
     if getattr(f, __CALLABLE_MODULE_PROP) == __RAN_AS_SCRIPT_MODULE:
         f()
     return f
+
+
+# Only export the main function
+__all__ = ["main"]
