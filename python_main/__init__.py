ebc2cf1cb49e5e44e544b3bfb64ea54a85c13ef0
Cadu <cadu.coelho@gmail.com>
2024-12-07 17:45:57 -0300

    Breaking change: Change the function name so that IDEs can properly suggest it. (#7)
    * Change the function name to be able to be more easily imported (some IDEs actively avoid suggesting importing something named "main")
    
    * Wordsmithing.
    
    * Wordsmithing #2

--- python_main/__init__.py
+++ python_main/__init__.py
@@ -6,11 +6,16 @@ __CALLABLE_MODULE_PROP = "__module__"
 __MAIN_RETURN_TYPE = TypeVar("__MAIN_RETURN_TYPE")
 
 
-def main(f: Callable[[], __MAIN_RETURN_TYPE]) -> Callable[[], __MAIN_RETURN_TYPE]:
-    if getattr(f, __CALLABLE_MODULE_PROP) == __RAN_AS_SCRIPT_MODULE:
-        f()
-    return f
+def python_main(
+    main_function_to_be_called: Callable[[], __MAIN_RETURN_TYPE]
+) -> Callable[[], __MAIN_RETURN_TYPE]:
+    if (
+        getattr(main_function_to_be_called, __CALLABLE_MODULE_PROP)
+        == __RAN_AS_SCRIPT_MODULE
+    ):
+        main_function_to_be_called()
+    return main_function_to_be_called
 
 
 # Only export the main function
-__all__ = ["main"]
+__all__ = ["python_main"]
